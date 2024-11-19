d9e4608ac2e2aeadf87aa69ef7cdbdc9703a6310
tonyissa <52683702+tonyissa@users.noreply.github.com>
2024-11-19 14:58:32 -0500

    Implement account linking with external auth providers

--- ExternalAuth.tonyissa/ProductManagementSystems.tonyissa/Program.cs
+++ ExternalAuth.tonyissa/ProductManagementSystems.tonyissa/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using ProductManagementSystems.tonyissa.Data;
 using ProductManagementSystems.tonyissa.Models;
 using ProductManagementSystems.tonyissa.Services;
@@ -31,30 +30,35 @@ builder.Services.AddAuthentication()
         options.AppId = builder.Configuration["FacebookSettings:AppId"]!;
         options.AppSecret = builder.Configuration["FacebookSettings:AppSecret"]!;
         options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+        options.Scope.Add("email");
     })
     .AddTwitter(options =>
     {
         options.ConsumerKey = builder.Configuration["TwitterSettings:ClientId"]!;
         options.ConsumerSecret = builder.Configuration["TwitterSettings:ClientSecret"]!;
         options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+        options.RetrieveUserDetails = true;
     })
     .AddGoogle(options =>
     {
         options.ClientId = builder.Configuration["GoogleSettings:ClientId"]!;
         options.ClientSecret = builder.Configuration["GoogleSettings:ClientSecret"]!;
         options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+        options.Scope.Add("email");
     })
     .AddMicrosoftAccount(options =>
     {
         options.ClientId = builder.Configuration["MicrosoftSettings:ClientId"]!;
         options.ClientSecret = builder.Configuration["MicrosoftSettings:ClientSecret"]!;
         options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+        options.Scope.Add("email");
     })
     .AddGitHub(options =>
     {
         options.ClientId = builder.Configuration["GithubSettings:ClientId"]!;
         options.ClientSecret = builder.Configuration["GithubSettings:ClientSecret"]!;
         options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+        options.Scope.Add("email");
     });
 
 builder.Services.AddAuthorizationBuilder()
